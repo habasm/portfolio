b24dedce7f8ab70e186eaf7276c2ad71aab49320
Roy Jumah <royjumah17@gmail.com>
2022-11-17 18:00:49 +0300

    Fix eslint errors

--- src/main.js
+++ src/main.js
@@ -1,92 +1,92 @@
 // Mobile menu functionality
-export const menuIconMobile = document.querySelector(".navbar__hamb");
-const menu = document.querySelector(".navbar__menu");
-export const menuLinks = document.querySelectorAll(".navbar__menu__link");
+export const menuIconMobile = document.querySelector('.navbar__hamb');
+const menu = document.querySelector('.navbar__menu');
+export const menuLinks = document.querySelectorAll('.navbar__menu__link');
 
-const isActiveClassExisted = (element) => element.classList.contains("active");
+const isActiveClassExisted = (element) => element.classList.contains('active');
 
 const refreshIcon = () => {
   if (isActiveClassExisted(menu)) {
-    menuIconMobile.setAttribute("src", "images/hamburguer-close.png");
+    menuIconMobile.setAttribute('src', 'images/hamburguer-close.png');
   } else {
-    menuIconMobile.setAttribute("src", "images/hamburguer.png");
+    menuIconMobile.setAttribute('src', 'images/hamburguer.png');
   }
 };
 export const openMenu = () => {
-  menu.classList.toggle("active");
+  menu.classList.toggle('active');
   refreshIcon();
 };
 
 export const closeMenu = () => {
-  menu.classList.remove("active");
+  menu.classList.remove('active');
   refreshIcon();
 };
 
 // Popup window functionality
 export const projects = [
   {
-    name: "Topic",
-    feature: "CANOPY",
-    background: "Back End Dev",
-    year: "2015",
-    image: "images/desktop/tonic-project-desktop.svg",
+    name: 'Topic',
+    feature: 'CANOPY',
+    background: 'Back End Dev',
+    year: '2015',
+    image: 'images/desktop/tonic-project-desktop.svg',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://habasm.github.io/portfolio/",
-    linkSource: "https://github.com/habasm",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://habasm.github.io/portfolio/',
+    linkSource: 'https://github.com/habasm',
   },
   {
-    name: "Multi-Post Stories",
-    feature: "FACEBOOK",
-    background: "Full Stack Dev",
-    year: "2015",
-    image: "images/desktop/multi-post-stories-project.svg",
+    name: 'Multi-Post Stories',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: 'images/desktop/multi-post-stories-project.svg',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://habasm.github.io/portfolio/",
-    linkSource: "https://github.com/habasm",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://habasm.github.io/portfolio/',
+    linkSource: 'https://github.com/habasm',
   },
   {
-    name: "Facebook 360",
-    feature: "FACEBOOK",
-    background: "Full Stack Dev",
-    year: "2015",
-    image: "images/desktop/facebook-360-project.svg",
+    name: 'Facebook 360',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: 'images/desktop/facebook-360-project.svg',
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://habasm.github.io/portfolio/",
-    linkSource: "https://github.com/habasm",
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://habasm.github.io/portfolio/',
+    linkSource: 'https://github.com/habasm',
   },
   {
-    name: "Uber Navigation",
-    feature: "Uber",
-    background: "Lead Developer",
-    year: "2018",
-    image: "images/desktop/uber-navigation-project.svg",
+    name: 'Uber Navigation',
+    feature: 'Uber',
+    background: 'Lead Developer',
+    year: '2018',
+    image: 'images/desktop/uber-navigation-project.svg',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    tool1: "html",
-    tool2: "css",
-    tool3: "javascript",
-    linkLive: "https://habasm.github.io/portfolio/",
-    linkSource: "https://github.com/habasm",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    tool1: 'html',
+    tool2: 'css',
+    tool3: 'javascript',
+    linkLive: 'https://habasm.github.io/portfolio/',
+    linkSource: 'https://github.com/habasm',
   },
 ];
 
-const startingPoint = document.querySelector(".hero");
+const startingPoint = document.querySelector('.hero');
 
 export function createModal() {
   startingPoint.insertAdjacentHTML(
-    "afterend",
+    'afterend',
     `<div class='overlay'>
     <section class='modal'>
     <div class='modal__header flex'>
@@ -119,125 +119,125 @@ export function createModal() {
     </li>
     </ul>
     </section>
-    </div>`
+    </div>`,
   );
 }
 
-menuIconMobile.addEventListener("click", openMenu);
+menuIconMobile.addEventListener('click', openMenu);
 menuLinks.forEach((menuLink) => {
-  menuLink.addEventListener("click", closeMenu);
+  menuLink.addEventListener('click', closeMenu);
 });
 
 // Event that detects when the page loads
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   createModal();
 
-  const buttonsProject = document.querySelectorAll(".works__card button");
-  const closeIcon = document.querySelector(".closeModal");
-  const modal = document.querySelector(".modal");
-  const backgroundModal = document.querySelector(".overlay");
-  const projectName = document.querySelector(".project-name");
-  const projectFeature = document.querySelector(".project-feature");
-  const projectBack = document.querySelector(".project-background");
-  const projectYear = document.querySelector(".project-year");
-  const projectImage = document.querySelector(".project-image");
-  const projectDescription = document.querySelector(".project-description");
-  const projectTool1 = document.querySelector(".project-tool1");
-  const projectTool2 = document.querySelector(".project-tool2");
-  const projectTool3 = document.querySelector(".project-tool3");
-  const projectLinkLive = document.querySelector(".project-linkLive");
-  const projectLinkSource = document.querySelector(".project-linkSource");
+  const buttonsProject = document.querySelectorAll('.works__card button');
+  const closeIcon = document.querySelector('.closeModal');
+  const modal = document.querySelector('.modal');
+  const backgroundModal = document.querySelector('.overlay');
+  const projectName = document.querySelector('.project-name');
+  const projectFeature = document.querySelector('.project-feature');
+  const projectBack = document.querySelector('.project-background');
+  const projectYear = document.querySelector('.project-year');
+  const projectImage = document.querySelector('.project-image');
+  const projectDescription = document.querySelector('.project-description');
+  const projectTool1 = document.querySelector('.project-tool1');
+  const projectTool2 = document.querySelector('.project-tool2');
+  const projectTool3 = document.querySelector('.project-tool3');
+  const projectLinkLive = document.querySelector('.project-linkLive');
+  const projectLinkSource = document.querySelector('.project-linkSource');
 
   const closeModal = () => {
-    modal.classList.remove("active");
-    backgroundModal.classList.remove("active");
+    modal.classList.remove('active');
+    backgroundModal.classList.remove('active');
   };
 
   buttonsProject.forEach((button) => {
-    button.addEventListener("click", () => {
-      if (button.id === "0") {
+    button.addEventListener('click', () => {
+      if (button.id === '0') {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute("src", `${projects[button.id].image}`);
+        projectImage.setAttribute('src', `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          "href",
-          `${projects[button.id].linkSource}`
+          'href',
+          `${projects[button.id].linkSource}`,
         );
-        modal.classList.add("active");
-        backgroundModal.classList.add("active");
-      } else if (button.id === "1") {
+        modal.classList.add('active');
+        backgroundModal.classList.add('active');
+      } else if (button.id === '1') {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute("src", `${projects[button.id].image}`);
+        projectImage.setAttribute('src', `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          "href",
-          `${projects[button.id].linkSource}`
+          'href',
+          `${projects[button.id].linkSource}`,
         );
-        modal.classList.add("active");
-        backgroundModal.classList.add("active");
-      } else if (button.id === "2") {
+        modal.classList.add('active');
+        backgroundModal.classList.add('active');
+      } else if (button.id === '2') {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute("src", `${projects[button.id].image}`);
+        projectImage.setAttribute('src', `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          "href",
-          `${projects[button.id].linkSource}`
+          'href',
+          `${projects[button.id].linkSource}`,
         );
-        modal.classList.add("active");
-        backgroundModal.classList.add("active");
-      } else if (button.id === "3") {
+        modal.classList.add('active');
+        backgroundModal.classList.add('active');
+      } else if (button.id === '3') {
         projectName.textContent = `${projects[button.id].name}`;
         projectFeature.textContent = `${projects[button.id].feature}`;
         projectBack.textContent = `${projects[button.id].background}`;
         projectYear.textContent = `${projects[button.id].year}`;
-        projectImage.setAttribute("src", `${projects[button.id].image}`);
+        projectImage.setAttribute('src', `${projects[button.id].image}`);
         projectDescription.textContent = `${projects[button.id].description}`;
         projectTool1.textContent = `${projects[button.id].tool1}`;
         projectTool2.textContent = `${projects[button.id].tool2}`;
         projectTool3.textContent = `${projects[button.id].tool3}`;
-        projectLinkLive.setAttribute("href", `${projects[button.id].linkLive}`);
+        projectLinkLive.setAttribute('href', `${projects[button.id].linkLive}`);
         projectLinkSource.setAttribute(
-          "href",
-          `${projects[button.id].linkSource}`
+          'href',
+          `${projects[button.id].linkSource}`,
         );
-        modal.classList.add("active");
-        backgroundModal.classList.add("active");
+        modal.classList.add('active');
+        backgroundModal.classList.add('active');
       }
     });
   });
 
-  closeIcon.addEventListener("click", closeModal);
+  closeIcon.addEventListener('click', closeModal);
 });
 
 // preserve data in the browser
 
 // declar constants for easy access
-const form = document.getElementById("myForm");
-const name = document.getElementById("fname");
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const getLocalStorage = localStorage.getItem("userProvidedInfo");
+const form = document.getElementById('myForm');
+const name = document.getElementById('fname');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const getLocalStorage = localStorage.getItem('userProvidedInfo');
 
 // Load to each contact form fields if there is pre-saved local storage data.
 if (getLocalStorage) {
@@ -247,13 +247,13 @@ if (getLocalStorage) {
   message.value = dataSave.message;
 }
 
-document.querySelectorAll("input, textarea").forEach((input) => {
-  input.addEventListener("input", (event) => {
+document.querySelectorAll('input, textarea').forEach((input) => {
+  input.addEventListener('input', (event) => {
     event.preventDefault();
     // Calling input values
-    const nameData = document.querySelector("#fname").value;
-    const emailData = document.querySelector("#email").value;
-    const msgData = document.querySelector("#message").value;
+    const nameData = document.querySelector('#fname').value;
+    const emailData = document.querySelector('#email').value;
+    const msgData = document.querySelector('#message').value;
 
     // Store values in object;
     const infoProvided = {
@@ -262,11 +262,11 @@ document.querySelectorAll("input, textarea").forEach((input) => {
       message: msgData,
     };
 
-    localStorage.setItem("userProvidedInfo", JSON.stringify(infoProvided));
+    localStorage.setItem('userProvidedInfo', JSON.stringify(infoProvided));
   });
 });
 
 // make the forma staying to the current load, for visualization or testing purpose
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
