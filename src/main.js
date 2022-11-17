51b92908f19ad95ad9b70ab313b7e2f0ed179383
Roy Jumah <royjumah17@gmail.com>
2022-11-17 17:47:21 +0300

    Load the data to the contact-field

--- src/main.js
+++ src/main.js
@@ -238,3 +238,35 @@ const name = document.getElementById("fname");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
 const getLocalStorage = localStorage.getItem("userProvidedInfo");
+
+// Load to each contact form fields if there is pre-saved local storage data.
+if (getLocalStorage) {
+  const dataSave = JSON.parse(getLocalStorage);
+  name.value = dataSave.name;
+  email.value = dataSave.email;
+  message.value = dataSave.message;
+}
+
+document.querySelectorAll("input, textarea").forEach((input) => {
+  input.addEventListener("input", (event) => {
+    event.preventDefault();
+    // Calling input values
+    const nameData = document.querySelector("#fname").value;
+    const emailData = document.querySelector("#email").value;
+    const msgData = document.querySelector("#message").value;
+
+    // Store values in object;
+    const infoProvided = {
+      name: nameData,
+      email: emailData,
+      message: msgData,
+    };
+
+    localStorage.setItem("userProvidedInfo", JSON.stringify(infoProvided));
+  });
+});
+
+// make the forma staying to the current load, for visualization or testing purpose
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
